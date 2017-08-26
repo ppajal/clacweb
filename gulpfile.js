5b22cc3360b933751ad5237473aeb4c744cc9ae6
ppajal <pajalic68@gmail.com>
2017-08-26 22:07:55 +1000

    remove old themes

--- gulpfile.js
+++ gulpfile.js
@@ -13,38 +13,38 @@ gulp.task('default', function() {
 });
 
 gulp.task('imagemin', function () {
-    return gulp.src('./themes/custom/clac/images/*')
+    return gulp.src('./themes/clac_subtheme/images/*')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
         }))
-        .pipe(gulp.dest('./themes/custom/clac/images'));
+        .pipe(gulp.dest('./themes/clac_subtheme/images'));
 });
 
 
 gulp.task('sass', function () {
-  gulp.src('./themes/custom/clac/sass/**/*.scss')
+  gulp.src('./themes/clac_subtheme/scss/**/*.scss')
     .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./themes/custom/clac/css'));
+    .pipe(gulp.dest('./themes/clac_subtheme/css'));
 });
 
 
 gulp.task('uglify', function() {
-  gulp.src('./themes/custom/clac/lib/*.js')
+  gulp.src('./themes/clac_subtheme/lib/*.js')
     .pipe(uglify('main.js'))
-    .pipe(gulp.dest('./themes/custom/clac/js'))
+    .pipe(gulp.dest('./themes/clac_subtheme/js'))
 });
 
 gulp.task('watch', function(){
     livereload.listen();
 
-    gulp.watch('./themes/custom/clac/sass/**/*.scss', ['sass']);
-    gulp.watch('./themes/custom/clac/lib/*.js', ['uglify']);
-    gulp.watch(['./themes/custom/clac/css/style.css', './themes/custom/clac/**/*.twig', './themes/custom/clac/js/*.js'], function (files){
+    gulp.watch('./themes/clac_subtheme/scss/**/*.scss', ['sass']);
+    gulp.watch('./themes/clac_subtheme/lib/*.js', ['uglify']);
+    gulp.watch(['./themes/clac_subtheme/css/style.css', './themes/clac_subtheme/templates/**/*.twig', './themes/clac_subtheme/js/*.js'], function (files){
         livereload.changed(files)
     });
 });
